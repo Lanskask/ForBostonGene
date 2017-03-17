212a298aabc4e34689b77217f2498fdb3f69ef3f
Lanskask <alanskaskiy@mail.ru>
2017-03-17 05:43:01 +0000

    A lot of changings

---
+++ SpringSecureMongoRest/SpringAndSecure/src/main/java/ru/smurtazin/forbostonegene/springandsecure/PersonRepository.java
@@ -0,0 +1,18 @@
+
+package ru.smurtazin.forbostonegene.springandsecure;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends MongoRepository<Person, String> {
+
+	List<Person> findByLastName(@Param("name") String name);
+
+	// ======= Mein ========
+	List<Person> findByEmail(@Param("email") String name);
+
+}
