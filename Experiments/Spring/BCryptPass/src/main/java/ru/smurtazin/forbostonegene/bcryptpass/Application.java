d4b02c05c42e98cf407ed2f68fa186c9f65b464d
Lanskask <alanskaskiy@mail.ru>
2017-03-17 19:52:25 +0000

    Commit in a middle of a big work. Smth works... basically not

---
+++ Experiments/Spring/BCryptPass/src/main/java/ru/smurtazin/forbostonegene/bcryptpass/Application.java
@@ -0,0 +1,15 @@
+
+package ru.smurtazin.forbostonegene.bcryptpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ Experiments/Spring/gs-accessing-mongodb-data-rest-complete/src/main/java/hello/PersonRepository.java
@@ -0,0 +1,23 @@
+
+package hello;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends MongoRepository<Person, String> {
+
+	List<Person> findByLastName(@Param("name") String name);
+
+	List<Person> findByEmail(@Param("email") String name);
+
+/*	void setBirthday(@Param("birthday") Date birthday);
+
+	void setEmail(@Param("email") String email);
+
+	void setPw_hash(@Param("password") String password);*/
+
+}
